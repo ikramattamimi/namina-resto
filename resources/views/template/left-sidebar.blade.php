dfb408c5b67fa4279fa39dc005032b8fce26907d
Ikram Muhammad Bukhori Attamimi <ikram.muhammad.tif20@polban.ac.id>
2023-09-07 20:51:10 +0700

    Install QR Package

--- resources/views/template/left-sidebar.blade.php
+++ resources/views/template/left-sidebar.blade.php
@@ -40,6 +40,13 @@
             <span>Rekening</span></a>
     </li>
 
+    <!-- Nav Item - Kode QR Meja -->
+    <li class="nav-item {{ str_contains(url()->current(), 'qrcode') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('qrcode.index') }}">
+            <i class="fas fa-fw fa-qrcode" aria-hidden="true"></i>
+            <span>Kode QR Meja</span></a>
+    </li>
+
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\QrCodeController;
 use App\Http\Controllers\RekeningController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,5 @@ Route::get('/', function () {
 Route::resource('pelanggan', PelangganController::class);
 
 Route::resource('rekening', RekeningController::class);
+
+Route::resource('qrcode', QrCodeController::class);
