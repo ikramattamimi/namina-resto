586390a7d769204a4cd10d8ad1888240ce81a768
unknown <muhamadrafli419@gmail.com>
2023-09-13 18:08:43 +0700

    pembelian

--- resources/views/bahanBaku/index.blade.php
+++ resources/views/bahanBaku/index.blade.php
@@ -20,37 +20,43 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th style="width:24.1719px">No.</th>
                             <th>Nama</th>
                             <th>Deskripsi</th>
                             <th>Harga Beli</th>
                             <th>Stok</th>
                             <th>Minimal Stok</th>
                             <th>Satuan</th>
-                            <th>Aksi</th>
+                            <th style="text-align:center; width:116.578px">Aksi</th>
                         </tr>
                     </thead>
                     <tfoot>
                         <tr>
+                            <th style="width:24.1719px">No.</th>
                             <th>Nama</th>
                             <th>Deskripsi</th>
                             <th>Harga Beli</th>
                             <th>Stok</th>
                             <th>Minimal Stok</th>
                             <th>Satuan</th>
-                            <th>Aksi</th>
+                            <th style="text-align:center; width:116.578px">Aksi</th>
                         </tr>
                     </tfoot>
                     <tbody>
-                    @foreach ($bahanBakus as $bahanBaku)
-                            <tr>
-                                <td>{{ $bahanBaku->nama }}</td>
-                                <td>{{ $bahanBaku->deskripsi }}</td>
-                                <td>{{ $bahanBaku->harga_beli }}</td>
-                                <td>{{ $bahanBaku->stok }}</td>
-                                <td>{{ $bahanBaku->minimal_stok }}</td>
-                                <td>{{ $bahanBaku->satuan }}</td>
-                                <td>{{ $bahanBaku->satuan }}</td>
-                            </tr>
+                        @foreach ($bahanBakus as $bahanBaku)
+                        <tr>
+                            <td>1</td>
+                            <td>{{ $bahanBaku->nama }}</td>
+                            <td>{{ $bahanBaku->deskripsi }}</td>
+                            <td>{{ $bahanBaku->harga_beli }}</td>
+                            <td>{{ $bahanBaku->stok }}</td>
+                            <td>{{ $bahanBaku->minimal_stok }}</td>
+                            <td>{{ $bahanBaku->satuan }}</td>
+                            <td><button type="button" class="btn btn-warning mr-1" title="Edit Status" data-toggle="modal" data-target="#ubahStatus"><i class="fas fa-pencil-alt fa-xs"></i></button>
+                                <a href="/order/pendingDanProses/edit" class="btn btn-primary mr-1" title="Edit Data"><i class="fas fa-edit fa-xs"></i></a>
+                                <a href="#" class="btn btn-success mr-1" title="Cetak Nota Proses"><i class="fas fa-print fa-xs"></i></a>
+                            </td>
+                        </tr>
                         @endforeach
                     </tbody>
                 </table>
