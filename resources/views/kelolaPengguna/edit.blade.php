137769466242bdaf0c830fcc9132d26d673ed75f
Muhamad Rafli Nur Ikhsan <80728109+Dudedu11@users.noreply.github.com>
2023-10-03 16:12:31 +0700

    fix select

--- resources/views/kelolaPengguna/edit.blade.php
+++ resources/views/kelolaPengguna/edit.blade.php
@@ -1,17 +1,18 @@
 <x-admin-layout headerTitle="Edit Data Pengguna">
 
     <x-card title="Data Pengguna">
-    <x-form action="{{ route('kelolaPengguna.update', $user) }}" method="PUT">
+        <x-form action="{{ route('kelolaPengguna.update', $user) }}" method="PUT">
             <x-input name="nama" type="text" value="{{ $user->nama }}" />
             <x-input name="username" type="text" value="{{ $user->username }}" />
             <x-input name="password" type="text" />
-            <label class="col-md-3 col-form-label text-md-end">Role</label>
-            <select name="roleId" id="rekeningId">
+            <x-select class="" name="roleId" label="Role">
                 <option value="{{ $user->role_id}}">{{ $user->role->nama}}</option>
-                @foreach($roles as $role)
-                <option value="{{ $role->id }}">{{ $role->nama }}</option>
+                @foreach ($roles as $role)
+                <option value="{{ $role->id }}">
+                    {{ $role->nama }}
+                </option>
                 @endforeach
-            </select>
+            </x-select>
         </x-form>
     </x-card>
 
--- resources/views/pengeluaranRestoran/create.blade.php
+++ resources/views/pengeluaranRestoran/create.blade.php
@@ -2,16 +2,17 @@
 
     <x-card title="Data Pengeluaran Restoran">
         <x-form action="{{ route('pengeluaranRestoran.store') }}" method="POST">
-            <x-input name="nama" type="text" label="Nama Pengeluaran"/>
+            <x-input name="nama" type="text" label="Nama Pengeluaran" />
             <x-input name="jumlah" type="number" />
             <x-input name="tanggal" type="date" />
-            <label class="col-md-4 col-form-label text-md-end">rekening</label>
-            <select name="rekeningId" id="rekeningId">
-                <option value="">-- Select --</option>
-                @foreach($rekenings as $rekening)
-                <option value="{{ $rekening->id }}">{{ $rekening->nama }}</option>
+            <x-select class="" name="rekeningId" label="Rekening">
+                <option value="">Pilih Rekening</option>
+                @foreach ($rekenings as $rekening)
+                <option value="{{ $rekening->id }}">
+                    {{ $rekening->nama }}
+                </option>
                 @endforeach
-            </select>
+            </x-select>
         </x-form>
     </x-card>
 
